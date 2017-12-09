9e78c1318bd7d5a57d303682c44ab019299d07e4
Malkaviel <maxime.capuano2@gmail.com>
2017-12-09 19:05:38 +0100

    Reduced the trait bound madness. Implemented Deref for PoolObjectHandler (simple wrapper around Rc<RefCell<PoolObject<T>>>). Implemented Debug for PoolObjectHandler. Implemented Deref for ObjectPool<T> (simple wrapper around Vec<PoolObjectHandler<T>>). Implemented ObjectPool<T>::nb_unused -> number of objects not used. Started implementing tests.

--- src/pool_allocator.rs
+++ src/pool_allocator.rs
@@ -88,48 +88,58 @@ use std::cell::RefCell;
 use std::fmt;
 use std::cmp;
 use std::slice::Iter;
-/*
-2 traits : Poolable, ConcurrentPoolable
-*/
+use std::ops;
 
+//TODO: l 138 : We should give an access to the inner object.
+//TODO: Finish the force_create with closure
+//TODO: finish the tests
+//TODO: Same but with Arc<Mutex<T>>
+//TODO: See if we can use the "free list" trick.
 //TODO: T : Send + Sync ? We should not manually implement them.
-//TODO: impl Sized ?
+//TODO: impl Sized ? ?Sized ?
 //Debug : Display some infos about the structure.
 //Default: Create our objects with a default configuration in the constructor of the ObjectPool
 //Ord : if the programmer asks for an object, but all objects are used, we may need to "kill" an object. We use the Ord trait to find the object to kill.
 
 //We use objects handlers to use a custom drop implementation.
-//TODO: test the strong count ! Apparently, calling std::mem::drop doesn't release borrows.
-//TODO: impl deref to obtain directly the Rc, which impl deref to obtain the T in Rc<T> ?
 
 #[derive(Default, Debug, Ord, PartialOrd, Eq, PartialEq)]
-pub struct PoolObjectHandler<T: Poolable>(Rc<RefCell<PoolObject<T>>>);
+pub struct PoolObjectHandler<T: Default>(Rc<RefCell<PoolObject<T>>>);
 
-impl<T: Poolable> Drop for PoolObjectHandler<T> {
-    fn drop(&mut self) {
-        self.0.borrow_mut().reinitialize();
+impl<T: Default> ops::Deref for PoolObjectHandler<T> {
+    type Target = Rc<RefCell<PoolObject<T>>>;
+
+    fn deref(&self) -> &Rc<RefCell<PoolObject<T>>> {
+        &self.0
     }
 }
 
-impl<T: Poolable> Clone for PoolObjectHandler<T> {
+impl<T: Default> Drop for PoolObjectHandler<T> {
+    fn drop(&mut self) {
+        self.borrow_mut().reinitialize();
+    }
+}
+
+impl<T: Default> Clone for PoolObjectHandler<T> {
     fn clone(&self) -> PoolObjectHandler<T> {
         PoolObjectHandler(self.0.clone())
     }
 }
 
 
-//TODO: for PoolObject and ConcurrentPoolObject
-pub trait Poolable : fmt::Debug + Default + Ord + Eq + PartialOrd + PartialEq {}
-
-
-//TODO: impl Debug, Default, Ord, PartialOrd, Eq, PartialEq
-#[derive(Debug)]
-pub struct PoolObject<T: Poolable> {
+pub struct PoolObject<T: Default> {
     object: T,
     in_use: bool,
 }
 
-impl<T: Poolable> PoolObject<T> {
+impl<T: Default + fmt::Debug> fmt::Debug for PoolObject<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Object from pool holding: {:?}. used: {:?}", self.object, self.in_use)
+    }
+}
+
+
+impl<T: Default> PoolObject<T> {
     fn reinitialize(&mut self) {
         self.object = T::default();
         self.in_use = false;
@@ -144,7 +154,7 @@ impl<T: Poolable> PoolObject<T> {
     }
 }
 
-impl<T: Poolable> Default for PoolObject<T> {
+impl<T: Default> Default for PoolObject<T> {
     fn default() -> Self {
         PoolObject {
             object: T::default(),
@@ -153,34 +163,41 @@ impl<T: Poolable> Default for PoolObject<T> {
     }
 }
 
-impl<T: Poolable> Ord for PoolObject<T> {
+impl<T: Default + Ord> Ord for PoolObject<T> {
     fn cmp(&self, other: &PoolObject<T>) -> cmp::Ordering {
         self.object.cmp(&other.object)
     }
 }
 
-impl<T: Poolable> PartialOrd for PoolObject<T> {
+impl<T: Default + PartialOrd> PartialOrd for PoolObject<T> {
     fn partial_cmp(&self, other: &PoolObject<T>) -> Option<cmp::Ordering> {
         self.object.partial_cmp(&other.object)
     }
 }
 
-impl<T: Poolable> PartialEq for PoolObject<T> {
+impl<T: Default + PartialEq> PartialEq for PoolObject<T> {
     fn eq(&self, other: &PoolObject<T>) -> bool {
         self.object.eq(&other.object)
     }
 }
 
-impl<T: Poolable> Eq for PoolObject<T> {}
+impl<T: Default + Eq > Eq for PoolObject<T> {}
 
 
 
 
-pub struct ObjectPool<T: Poolable> {
-    objects: Vec<PoolObjectHandler<T>>,
+impl<T: Default> ops::Deref for ObjectPool<T> {
+    type Target = Vec<PoolObjectHandler<T>>;
+
+    fn deref(&self) -> &Vec<PoolObjectHandler<T>> {
+        &self.0
+    }
 }
 
-impl<T: Poolable> ObjectPool<T> {
+pub struct ObjectPool<T: Default>(Vec<PoolObjectHandler<T>>);
+
+
+impl<T: Default> ObjectPool<T> {
     pub fn with_capacity(size: usize) -> Self {
         let mut objects = Vec::with_capacity(size);
 
@@ -188,47 +205,126 @@ impl<T: Poolable> ObjectPool<T> {
             objects.push(PoolObjectHandler::default());
         }
 
-        ObjectPool {
-            objects,
-        }
+        ObjectPool(objects)
 
     }
 
     pub fn create(&self) -> Option<PoolObjectHandler<T>> {
-         match self.objects.iter().find(|obj| {!obj.0.borrow_mut().is_used()}) {
+         match self.iter().find(|obj| {!obj.borrow_mut().is_used()}) {
              Some(obj_ref) => {
-                 obj_ref.0.borrow_mut().set_used(true);
+                 obj_ref.borrow_mut().set_used(true);
                  Some(obj_ref.clone())
              },
              None => None,
          }
     }
 
+    //TODO: we need a closure as arg.
+    pub fn force_create_with_filter(&mut self) -> Option<PoolObjectHandler<T>> {
+unimplemented!()
+    }
+
+
+
+    pub fn nb_unused(&self) -> usize {
+        self.iter().filter(|obj| !obj.0.borrow_mut().is_used()).count()
+    }
+}
+
+impl<T: Default + Ord> ObjectPool<T> {
     pub fn force_create(&mut self) -> Option<PoolObjectHandler<T>> {
-        match self.objects.iter().min() {
+        match self.iter().min() {
             Some(obj_ref) => {
-                obj_ref.0.borrow_mut().reinitialize();
-                obj_ref.0.borrow_mut().set_used(true);
+                obj_ref.borrow_mut().reinitialize();
+                obj_ref.borrow_mut().set_used(true);
                 Some(obj_ref.clone())
             },
             None => None,
         }
     }
-
-    pub fn iter(&self) -> Iter<PoolObjectHandler<T>> {
-        self.objects.iter()
-    }
-
-    pub fn len(&self) -> usize {
-        self.objects.len()
-    }
 }
-
 
 #[cfg(test)]
 mod objectpool_tests {
     use super::*;
 
 
+    #[derive(Ord, PartialOrd, Eq, PartialEq, Debug)]
+    struct Monster {
+        name: String,
+        level: u8,
+        hp: u32,
+    }
 
+    impl Default for Monster {
+        fn default() -> Self {
+            Monster {
+                name: String::from("default name"),
+                level: 1,
+                hp: 10,
+            }
+        }
+    }
+
+    #[test]
+    fn test_len() {
+        let simple_pool: ObjectPool<u8> = ObjectPool::with_capacity(26);
+        assert_eq!(simple_pool.len(), 26);
+        assert_eq!(simple_pool.len(), simple_pool.capacity())
+    }
+
+    fn test_is_used_at_initialization() {
+        let monster_pool: ObjectPool<Monster> = ObjectPool::with_capacity(14);
+        for monster in monster_pool.iter() {
+            assert!(!monster.borrow_mut().is_used())
+        }
+    }
+
+    #[test]
+    fn test_drop_wrapper_around_smart_pointer() {
+        let monster_pool: ObjectPool<Monster> = ObjectPool::with_capacity(10);
+        let monster = monster_pool.create().unwrap();
+        assert_eq!(Rc::strong_count(&monster), 2);
+        assert!(monster.borrow_mut().is_used());
+        assert_eq!(monster_pool.nb_unused(), 9);
+        {
+            let monster2 = monster_pool.create().unwrap();
+            assert_eq!(Rc::strong_count(&monster2), 2);
+            assert!(monster2.borrow_mut().is_used());
+            assert_eq!(monster_pool.nb_unused(), 8);
+
+            //monster2 will be dropped here, we must check :
+            // - nb_unused() returns 9. It will mean that our drop implementation for the wrapper
+            //around the Rc<RefCell<T>> works.
+
+            // - every strong count should be 1 and each object should have in_use to false.
+            // except for monster.
+        }
+        assert_eq!(monster_pool.nb_unused(), 9);
+        let nb_monster_with_1_ref = monster_pool
+            .iter()
+            .filter(|obj| {
+                Rc::strong_count(&obj) == 1
+            }).count();
+
+        assert_eq!(nb_monster_with_1_ref, 9);
+
+        let nb_monster_unused = monster_pool
+            .iter()
+            .filter(|obj| {
+              !obj.borrow_mut().is_used()
+        }).count();
+
+        assert_eq!(nb_monster_unused, 9);
+    }
+
+    #[test]
+    fn test_create_no_more_objects() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn test_force_create() {
+        unimplemented!()
+    }
 }
